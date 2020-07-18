8f01721e5ada2a481c83767b41ad9475dbe153dd
Sarra-Boufedji <sarraboufedji@gmail.com>
2020-07-18 23:21:48 +0100

    Including print in input month

--- Bikeshare.py
+++ Bikeshare.py
@@ -22,11 +22,9 @@ def get_filters():
         city = input("Please enter one city:chicago, new york city or washington: ").lower()
 
     # get user input for month (all, january, february, ... , june)
-    print("please choose the month you want to analyze or all for no month filter: ")
-    month = input().lower()
+    month = input("please choose the month you want to analyze or all for no month filter: ").lower()
     while month not in ('january','february','march','april','may','june','all'):
-        print('Please choose one month from this list : january, february, march, april, may, june')
-        month=input()
+        month=input('Please choose one month from this list : january, february, march, april, may, june').lower()
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
     print("please choose the day to analyze or all for no day filter: ")
